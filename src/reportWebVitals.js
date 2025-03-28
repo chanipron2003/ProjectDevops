7f4798277b0bce95335041fbc50fdec6462aff20
NiceVani <Nice090147022165@gmail.com>
2025-03-28 16:41:09 +0700

    update

---
+++ src/reportWebVitals.js
@@ -0,0 +1,7 @@
+// src/reportWebVitals.js
+const reportWebVitals = (metric) => {
+    console.log(metric);  // สามารถเก็บข้อมูลหรือส่งไปยังบริการภายนอกได้
+  };
+  
+  export default reportWebVitals;
+  
